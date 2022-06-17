278d17d896eb312507e7f91171ea8c9b0628a4ff
RuiOkazaki <okazaki200226@gmail.com>
2022-06-17 11:49:22 +0900

    feat: cli cmd を受け取れる

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,31 @@
 use std::path::PathBuf;
+use structopt::StructOpt;
 
+#[derive(Debug, StructOpt)]
 pub enum Action {
     // Action::Add -> "buy milk!"
-    Add { task: String },
+    Add {
+        #[structopt()]
+        text: String,
+    },
     // Action::Done -> task number を受け取り、完了済みにする
-    Done { position: usize },
+    Done {
+        #[structopt()]
+        position: usize,
+    },
     // Action::Done -> タスクをリストで表示する
     List,
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "rust-todo",
+    about = "command line で使用できる todo アプリケーション"
+)]
 pub struct CommandLineArgs {
+    #[structopt(subcommand)]
     pub action: Action,
     // optionalにする
+    #[structopt(parse(from_os_str), short, long)]
     pub journal_file: Option<PathBuf>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 mod cli;
+use structopt::StructOpt;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:#?}", cli::CommandLineArgs::from_args());
+    ////// cargo run add "buy milk!"
+    // CommandLineArgs {
+    //     action: Add {
+    //         text: "buy milk",
+    //     },
+    //     journal_file: None,
+    // }
+
+    ////// cargo run done 12
+    // CommandLineArgs {
+    //     action: Done {
+    //         position: 12,
+    //     },
+    //     journal_file: None,
+    // }
+
+    ////// cargo run list
+    // CommandLineArgs {
+    //     action: List,
+    //     journal_file: None,
+    // }
+
+    ////// cargo run -- -j sample.txt list
+    // CommandLineArgs {
+    //     action: List,
+    //     journal_file: Some(
+    //         "sample.txt",
+    //     ),
+    // }
 }
