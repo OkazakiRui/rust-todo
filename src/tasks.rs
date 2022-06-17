3792d3944d7822bd3a3199f9fcc5d41c95625d95
RuiOkazaki <okazaki200226@gmail.com>
2022-06-17 18:53:07 +0900

    feat: task::new

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,7 +1,19 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Local>,
+}
+
+// Task::new が使用できるようになる
+impl Task {
+    pub fn new(text: String) -> Task {
+        let created_at: DateTime<Local> = Local::now();
+        Task { text, created_at }
+    }
 }
